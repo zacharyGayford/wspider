826e867af8bc47f2951b75ae46ab9c2ebbee87ba
zacharyGayford <zachary.gayford@gmail.com>
2023-09-03 19:50:38 -0500

    changed build script

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ cls
 if not exist bin mkdir bin
 set target=%1
 if [%target%]==[] (
-    set target=main
+    set target=default
 )
 
-cmake -B bin -S . --preset default
+cmake -B bin -S . --preset %target%
 move bin/compile_commands.json src
 
 ninja -C bin && .\bin\main.exe
